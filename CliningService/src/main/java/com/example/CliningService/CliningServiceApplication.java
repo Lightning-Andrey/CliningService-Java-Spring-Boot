3ae6049717d983d210fcafc51ccf329e8961acbd
Даниил <90754791+Heimdall-Program@users.noreply.github.com>
2023-05-18 13:11:56 +0300

    init

---
+++ CliningService/src/main/java/com/example/CliningService/CliningServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.CliningService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliningServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliningServiceApplication.class, args);
+	}
+
+}
---
+++ CliningService/src/test/java/com/example/CliningService/CliningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CliningService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliningServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
