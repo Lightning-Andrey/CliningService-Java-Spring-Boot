e411292cd6d6ec5c9709c44363e37f648cc92d83
Даниил <90754791+Heimdall-Program@users.noreply.github.com>
2023-05-29 10:26:44 +0300

    Update table-requests

--- CliningService/src/main/java/com/example/CleaningService/Controllers/CleaningRequestController.java
+++ CliningService/src/main/java/com/example/CleaningService/Controllers/CleaningRequestController.java
@@ -5,11 +5,9 @@ import com.example.CleaningService.Repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -18,18 +16,42 @@ public class CleaningRequestController {
     @Autowired
     private CleaningRequestRepository cleaningRequestRepository;
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private InventoryRepository inventoryRepository;
+
     @GetMapping("/table-requests")
     public String showCleaningRequests(Model model) {
 
         List<CleaningRequest> requests = cleaningRequestRepository.findAll();
+        List<Employee> employees = employeeRepository.findAll();
+        List<Inventory> inventories = inventoryRepository.findAll();
+        List<String> statusList = Arrays.asList("В обработке", "На согласовании", "Назначено", "Завершён");
+
         model.addAttribute("requests", requests);
+        model.addAttribute("employees", employees);
+        model.addAttribute("inventories", inventories);
+        model.addAttribute("statusList", statusList);
+
         return "table-requests";
     }
 
-    @PostMapping("/edit-request")
-    public String updateRequest(@ModelAttribute CleaningRequest request) {
-        cleaningRequestRepository.save(request);
-        return "redirect:/table-requests";
+    @GetMapping("/edit-request/{id}")
+    public String editRequest(@PathVariable("id") long id, Model model) {
+        CleaningRequest request = cleaningRequestRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid request Id:" + id));
+        List<Employee> employees = employeeRepository.findAll();
+        List<Inventory> inventories = inventoryRepository.findAll();
+        List<String> statusList = Arrays.asList("В обработке", "На согласовании", "Назначено", "Завершён");
+
+        model.addAttribute("request", request);
+        model.addAttribute("employees", employees);
+        model.addAttribute("inventories", inventories);
+        model.addAttribute("statusList", statusList);
+
+        return "edit-request";
     }
 
     @GetMapping("/delete-request/{id}")
